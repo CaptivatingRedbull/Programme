5606f3b952f181576a5a49f78c9b4e3033eeddc7
Redbull <76742406+CaptivatingRedbull@users.noreply.github.com>
2023-12-18 12:45:28 +0100

    Programme

--- src/Kontofuehrung.java
+++ src/Kontofuehrung.java
@@ -1,23 +1,30 @@
 public class Kontofuehrung {
     public static void main(String[] args) {
         GiroKonto giroKonto = new GiroKonto("5500328876100", 1000F);
+        SparKonto sparKonto = new SparKonto("5500328876101", 0.03F);
         System.out.println(giroKonto);
+        System.out.println(sparKonto);
     }
 }
 
 abstract class Konto {
     float ktoStand;
     String ktoNummer;
-    public Konto(String ktoNummer){
+
+    public Konto(String ktoNummer) {
         this.ktoNummer = ktoNummer;
         this.ktoStand = 0;
     }
+
     boolean einzahlen(float betrag) {
         if (betrag <= 0)
             return false;
         ktoStand += betrag;
         return true;
     }
+
+    abstract String kennung();
+
 
     @Override
     public String toString() {
@@ -28,15 +35,20 @@ abstract class Konto {
     }
 }
 
-class SparKonto extends Konto{
+class SparKonto extends Konto {
     float zins;
-    public SparKonto(String ktonummer, float zins){
+
+    public SparKonto(String ktonummer, float zins) {
         super(ktonummer);
         this.zins = zins;
 
     }
 
-    void annualZins(){
+    String kennung() {
+        return "SparKonto";
+    }
+
+    void annualZins() {
         ktoStand += ktoStand * zins;
     }
 
@@ -50,12 +62,16 @@ class SparKonto extends Konto{
     }
 }
 
-class GiroKonto extends Konto{
+class GiroKonto extends Konto {
     float dispo;
 
-    public GiroKonto(String ktoNummer, float dispo){
+    public GiroKonto(String ktoNummer, float dispo) {
         super(ktoNummer);
         this.dispo = dispo;
+    }
+
+    String kennung() {
+        return "GiroKonto";
     }
 
     boolean abheben(float betrag) {
