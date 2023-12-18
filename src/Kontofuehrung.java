63f8c04c3f178ff1cb39c032669b9dfbfa7aacf8
Redbull <76742406+CaptivatingRedbull@users.noreply.github.com>
2023-12-18 12:41:22 +0100

    Programme

--- src/Kontofuehrung.java
+++ src/Kontofuehrung.java
@@ -1,21 +1,60 @@
 public class Kontofuehrung {
     public static void main(String[] args) {
         GiroKonto giroKonto = new GiroKonto("5500328876100", 1000F);
+        System.out.println(giroKonto);
     }
 }
 
 abstract class Konto {
-
-}
-
-class GiroKonto {
     float ktoStand;
     String ktoNummer;
+    public Konto(String ktoNummer){
+        this.ktoNummer = ktoNummer;
+        this.ktoStand = 0;
+    }
+    boolean einzahlen(float betrag) {
+        if (betrag <= 0)
+            return false;
+        ktoStand += betrag;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Konto{" +
+                "ktoStand=" + ktoStand +
+                ", ktoNummer='" + ktoNummer + '\'' +
+                '}';
+    }
+}
+
+class SparKonto extends Konto{
+    float zins;
+    public SparKonto(String ktonummer, float zins){
+        super(ktonummer);
+        this.zins = zins;
+
+    }
+
+    void annualZins(){
+        ktoStand += ktoStand * zins;
+    }
+
+    @Override
+    public String toString() {
+        return "SparKonto{" +
+                "zins=" + zins +
+                ", ktoStand=" + ktoStand +
+                ", ktoNummer='" + ktoNummer + '\'' +
+                '}';
+    }
+}
+
+class GiroKonto extends Konto{
     float dispo;
 
     public GiroKonto(String ktoNummer, float dispo){
-        this.ktoNummer = ktoNummer;
-        this.ktoStand = 0;
+        super(ktoNummer);
         this.dispo = dispo;
     }
 
@@ -23,13 +62,6 @@ class GiroKonto {
         if (ktoStand + dispo < betrag || betrag <= 0)
             return false;
         ktoStand -= betrag;
-        return true;
-    }
-
-    boolean einzahlen(float betrag) {
-        if (betrag <= 0)
-            return false;
-        ktoStand += betrag;
         return true;
     }
 
